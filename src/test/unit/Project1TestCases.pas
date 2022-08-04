22aecc89761b4724080de6fba246f05ca7280010
Rodrigo <suporte2@newcap.com.br>
2022-08-04 14:29:52 -0300

    Add two new tests, TestPopulateStringList and TestSortStringList.

--- src/test/unit/Project1TestCases.pas
+++ src/test/unit/Project1TestCases.pas
@@ -3,14 +3,19 @@ unit Project1TestCases;
 interface
 
 uses
-  TestFramework;
+  TestFramework,
+  Classes;
 
 type
   TTestCaseFirst = class(TTestCase)
+  private
+    FStrList: TStringList;
   published
     procedure TestFirst;
     procedure TestSecond;
     procedure TestThird;
+    procedure TestPopulateStringList;
+    procedure TestSortStringList;
   end;
 
 implementation
@@ -31,6 +36,39 @@ var
 begin
   I := 0;
   Check(1 div I = I, 'Deliberate exception');
+end;
+
+procedure TTestCaseFirst.TestPopulateStringList;
+var
+  I: Integer;
+begin
+  try
+    FStrList := TStringList.Create;
+    Check(FStrList.Count = 0);
+    for I := 1 to 50 do
+      FStrList.Add('i');
+    Check(FStrList.Count = 50);
+  finally
+    FStrList.Free;
+  end;
+end;
+
+procedure TTestCaseFirst.TestSortStringList;
+begin
+  try
+    FStrList := TStringList.Create;
+    Check(FStrList.Sorted = False);
+    Check(FStrList.Count = 0);
+    FStrList.Add('You');
+    FStrList.Add('Love');
+    FStrList.Add('I');
+    FStrList.Sorted := True;
+    Check(FStrList[2] = 'You');
+    Check(FStrList[1] = 'Love');
+    Check(FStrList[0] = 'I');
+  finally
+    FStrList.Free;
+  end;
 end;
 
 initialization
