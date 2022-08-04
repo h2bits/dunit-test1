e0697ea0990d99ec3b9b522821e3542f095ec888
Rodrigo <suporte2@newcap.com.br>
2022-08-04 10:29:19 -0300

    Add the methods TestSecond and TestThird to illustrate a failure and an exception, respectivelly.

--- src/test/unit/Project1TestCases.pas
+++ src/test/unit/Project1TestCases.pas
@@ -9,6 +9,8 @@ type
   TTestCaseFirst = class(TTestCase)
   published
     procedure TestFirst;
+    procedure TestSecond;
+    procedure TestThird;
   end;
 
 implementation
@@ -16,6 +18,19 @@ implementation
 procedure TTestCaseFirst.TestFirst;
 begin
   Check(1 + 1 = 2, 'Catastrophic arithmetic failure!');
+end;
+
+procedure TTestCaseFirst.TestSecond;
+begin
+  Check(1 + 1 = 3, 'Deliberate failure');
+end;
+
+procedure TTestCaseFirst.TestThird;
+var
+  I: Integer;
+begin
+  I := 0;
+  Check(1 div I = I, 'Deliberate exception');
 end;
 
 initialization
